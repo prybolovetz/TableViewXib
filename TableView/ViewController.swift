069316bafe142ba03722a926228e5024ee93173c
Ivan Prybolovetz <ivan.prybo@gmail.com>
2018-12-07 02:24:24 +0300

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Ivan on 12/7/18.
+//  Copyright © 2018 Ivan Prybolovetz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
